dd0a64c9a6fb13b436933e2ca6e33632e079018a
Shahad Hassan <shahadg1983@gmail.com>
2024-03-24 14:19:57 +0530

    Updating app name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <div className="flex flex-col md:flex-row items-center justify-around p-5 md:p-10 md:mt-10">
             <ul className="flex flex-col md:flex-row items-center gap-x-10 mb-5 md:mb-0">
-                <li><Link to='/' className="font-bold">Reco<span className="font-bold text-yellow-300">Bee</span> </Link>2024. All rights Reserved</li>
+                <li><Link to='/' className="font-bold">Cine<span className="font-bold text-yellow-300">Quest</span> </Link>2024. All rights Reserved</li>
                 <li>Privacy Policy</li>
                 <li>Terms of Services</li>
             </ul>
