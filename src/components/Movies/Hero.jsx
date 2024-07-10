e2aa6c41cf8e964782e6049fdaccceb09570a5c3
Shahad Hassan <shahadg1983@gmail.com>
2024-07-10 11:51:11 +0530

    Resolving bugs

--- src/components/Movies/Hero.jsx
+++ src/components/Movies/Hero.jsx
@@ -58,7 +58,7 @@ export default function Hero() {
             <h1 className="p-5">Trending Movies on <span>Cine<span className="text-yellow-300">Quest</span></span></h1>
             <div className="flex flex-col md:flex-wrap md:flex-row items-center justify-around">
                 {
-                    Array.isArray(data) && data[0].slice(0, 6).map((item, index) => (
+                    Array.isArray(data) && data[0]?.slice(0, 6).map((item, index) => (
                         <div className="card cursor-pointer" key={index}>
                         <div  onClick={handleClick} id={`${item.id}`}  className="card2">
                             <img src={`https://image.tmdb.org/t/p/w500${item.poster_path}`} className="w-40 h-60 rounded-sm" />
